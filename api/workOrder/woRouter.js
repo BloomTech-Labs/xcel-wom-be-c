d02c4e2ab6dd9e7c6aa80b1331457f52d4d10e6b
unknown <tommyconner96@gmail.com>
2021-01-05 20:31:52 -0500

    added imports and basic file scaffold

---
+++ api/workOrder/woRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const authRequired = require('../middleware/authRequired');
+const workOrders = require('./woModel');
+const router = express.Router;
+
+// get all WO's
+router.get('/', authRequired, function (req, res) {});
+
+// get WO by id
+
+// CREATE wo
+
+module.exports = router;
