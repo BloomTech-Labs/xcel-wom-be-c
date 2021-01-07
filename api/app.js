578c7d2d61245db963f7f388b6a27be8e1fffafe
unknown <tommyconner96@gmail.com>
2021-01-06 21:04:10 -0500

    WO migration fixed, app.js updated with orders route, get all WOs working

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ const swaggerUIOptions = {
 //###[  Routers ]###
 const indexRouter = require('./index/indexRouter');
 const profileRouter = require('./profile/profileRouter');
-const woRouter = require('./workOrder/woRouter')
+const woRouter = require('./workOrder/woRouter');
 
 const app = express();
 
@@ -50,7 +50,7 @@ app.use(cookieParser());
 // application routes
 app.use('/', indexRouter);
 app.use(['/profile', '/profiles'], profileRouter);
-app.use('/orders/', woRouter)
+app.use('/orders/', woRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- data/migrations/20201217211445_workorder.js
+++ data/migrations/20201217211445_workorder.js
@@ -3,20 +3,17 @@ exports.up = (knex) => {
     table.integer('id').notNullable().unique().primary();
     table.uuid('uuid');
     table.string('requestId');
-    table
-      .foreign('assignedTo', 'profile_id')
-      .references('id')
-      .inTable('profile');
+    table.string('assignedTo');
+    table.foreign('assignedTo').references('id').inTable('profiles');
     table.string('incLocation').notNullable();
     table.string('unitAddress').notNullable();
-    table.dateTime('dateCreated').defaultTo(knex.function.now());
+    table.dateTime('dateCreated').defaultTo(knex.fn.now());
     table.dateTime('dateClosed');
     table.string('description').notNullable();
     table.string('priority').defaultTo(null);
     table.string('status').defaultTo('unassigned');
   });
 };
-
 exports.down = function (knex) {
   return knex.schema.dropTableIfExists('workOrders');
 };
