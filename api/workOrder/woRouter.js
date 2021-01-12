793b9449014391b34753a4868474ecb0e05f8589
Tommy Conner <56052655+tommyconner96@users.noreply.github.com>
2021-01-11 19:59:24 -0500

    Tc feature get all work orders (#29)
    * get all work orders route
    
    * get all WOs first draft
    
    * WO migration fixed, app.js updated with orders route, get all WOs working

--- api/workOrder/woRouter.js
+++ api/workOrder/woRouter.js
@@ -1,10 +1,20 @@
 const express = require('express');
 // const authRequired = require('../middleware/authRequired');
-// const workOrders = require('./woModel');
-const router = express.Router;
+const workOrders = require('./woModel');
+const router = express.Router();
 
 // get all WO's
-// router.get('/', authRequired, function (req, res) {});
+router.get('/', function (req, res) {
+  workOrders
+    .findAll()
+    .then((orders) => {
+      res.status(200).json(orders);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: err.message });
+    });
+});
 
 // get WO by id
 
