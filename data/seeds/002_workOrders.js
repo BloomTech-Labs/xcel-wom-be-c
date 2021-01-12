f8a9a34cd9bf261c662e439219acf8b4b142e92d
Sophia Jung <sophiabraddock82@gmail.com>
2021-01-11 20:02:59 -0500

    sj-feat-seeds

--- data/seeds/002_workOrders.js
+++ data/seeds/002_workOrders.js
@@ -2,14 +2,14 @@ const faker = require('faker');
 
 const workorder = [
   {
-    
+    id: 3,
     uuid: null,
-    requestId: 'faker.random.alphaNumeric(10)',
+    requestId: faker.random.alphaNumeric(10),
     assignedTo: '00ulthapbErVUwVJy4x6',
     incLocation: 'kitchen', // incident location
-    unitAddress: faker.address.secondaryAddress, // for specific apartment unit not full address location
-    dateCreated: faker.date.soon,
-    dateClosed: faker.date.future,
+    unitAddress: faker.address.secondaryAddress(), // for specific apartment unit not full address location
+    dateCreated: faker.date.past(),
+    dateClosed: faker.date.future(),
     description: 'Oven is not working',
     priority: '1',
     status: 'assigned',
@@ -18,10 +18,10 @@ const workorder = [
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('workOrders')
+  return knex("workOrders")
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('workOrders').insert(workorder);
+      return knex("workOrders").insert(workorder);
     });
 };
