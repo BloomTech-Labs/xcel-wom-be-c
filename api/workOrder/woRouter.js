d09d783ca52c946bf0b7e1b02a764a03b6412e6d
Sophia Jung <34404684+sophiasagan@users.noreply.github.com>
2021-01-12 20:11:55 -0500

    sj-feat-create workOrder routes (#31)

--- api/workOrder/woRouter.js
+++ api/workOrder/woRouter.js
@@ -19,5 +19,32 @@ router.get('/', function (req, res) {
 // get WO by id
 
 // CREATE wo
+router.post('/', async (req, res) => {
+  const order = req.body;
+  if (order) {
+    const id = order.id || 0;
+    try {
+      await workOrders.findById(id).then(async (pf) => {
+        if (pf == undefined) {
+          //workOrder not found so lets insert it
+          await workOrders
+            .create(order)
+            .then((order) =>
+              res
+                .status(200)
+                .json({ message: 'Work Order created', workorder: order[0] })
+            );
+        } else {
+          res.status(400).json({ message: 'Work Order already exists' });
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    res.status(404).json({ message: 'Work Order missing' });
+  }
+});
 
 module.exports = router;
