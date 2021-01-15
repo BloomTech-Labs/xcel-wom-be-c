a3be2d30f0dac1b150fd1ab02aad169c68685e91
unknown <tommyconner96@gmail.com>
2021-01-14 21:13:15 -0500

    tc - more route tests written

--- __tests__/routes/workOrders.test.js
+++ __tests__/routes/workOrders.test.js
@@ -24,4 +24,32 @@ describe('profiles router endpoints', () => {
       expect(workOrders.findAll.mock.calls.length).toBe(1);
     });
   });
+  describe('GET /orders/:orderId', () => {
+    it('returns status 200 if wo exists', async () => {
+      // mock data matches seed data so we know it will exist for the test
+      workOrders.findById.mockResolvedValue({
+        id: 3,
+        uuid: null,
+        assignedTo: '00ulthapbErVUwVJy4x6',
+        incLocation: 'kitchen',
+        unitAddress: 'Suite 061',
+        dateCreated: '2020-05-29T18:11:06.712Z',
+        dateClosed: '2021-09-13T09:20:51.968Z',
+        description: 'Oven is not working',
+        priority: '1',
+        status: 'assigned',
+      });
+      const res = await request(app).get('/orders/4');
+
+      expect(res.status).toBe(200);
+      expect(res.body.assignedTo).toBe('00ulthapbErVUwVJy4x6');
+      expect(workOrders.findById.mock.calls.length).toBe(1);
+    });
+
+    it('should return 404 when no user found', async () => {
+      workOrders.findById.mockResolvedValue();
+      const res = await request(app).get('/orders/fake12345');
+      expect(res.status).toBe(404);
+    });
+  });
 });
