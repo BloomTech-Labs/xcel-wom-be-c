09e54d3ef0a06f45a5c2fb597e2085df184f4d6a
Sophia Jung <sophiabraddock82@gmail.com>
2021-01-21 20:17:02 -0500

    sj feature work order router testing

--- __tests__/routes/workOrders.test.js
+++ __tests__/routes/workOrders.test.js
@@ -52,4 +52,71 @@ describe('profiles router endpoints', () => {
       expect(res.status).toBe(404);
     });
   });
+
+  describe('POST /orders', () => {
+    it('should return 200 when work order is created', async () => {
+      const order = {
+        uuid: null,
+        assignedTo: '00ulthapbErVUwVJy4x6',
+        incLocation: 'bedroom',
+        unitAddress: 'Suite 461',
+        dateCreated: '2020-09-29T18:11:06.712Z',
+        dateClosed: '2021-04-13T09:20:51.968Z',
+        description: 'Light switch is not working',
+        priority: '1',
+        status: 'assigned',
+      };
+
+      workOrders.create.mockResolvedValue(order);
+      const res = await request(app).post('/orders').send(order);
+
+      expect(res.status).toBe(200);
+
+      expect(res.body.message).toBe('Work Order created');
+      expect(res.body.message).toBeTruthy();
+
+      expect(workOrders.create.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('PUT /orders/:id', () => {
+    it('should return 200 when order is updated', async () => {
+      const order = {
+        incLocation: 'bedroom',
+        unitAddress: 'Suite 461',
+        dateCreated: '2020-09-29T18:11:06.712Z',
+        dateClosed: '2021-04-13T09:20:51.968Z',
+        description: 'Light switch is not working',
+      };
+      workOrders.findById.mockResolvedValue(order);
+      workOrders.update.mockResolvedValue([order]);
+
+      const res = await request(app).put('/orders/3').send(order);
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe('Successfully updated order 3');
+      expect(res.body.message).toBeTruthy();
+
+      expect(workOrders.update.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('DELETE /orders', () => {
+    it('should return 200 when order is deleted ', async () => {
+      const order = {
+        id: 3,
+      };
+      workOrders.findById.mockResolvedValue([order]);
+      workOrders.remove.mockResolvedValue(1);
+
+      const res = await request(app).delete(`/orders/${order.id}`).send();
+
+      // Grab the first element in the response
+      const first = res.body[0];
+      expect(first).toBeUndefined();
+
+      expect(res.body.message).toBe('work Order 3 was deleted.');
+
+      expect(workOrders.remove.mock.calls.length).toBe(1);
+    });
+  });
 });
